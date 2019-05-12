36082c2efa8aad90498f72e66cdbac15e7e84225
Nicole Salazar <dn.salazar10@uniandes.edu.co>
2019-05-11 23:58:29 -0500

    make file de la tarea

---
+++ makehw.mk
@@ -0,0 +1,11 @@
+Resultados_hw2.pdf: Senal.png #TransformadasSenales.png amplitud1.png amplitud2.png amplitud3.png amplitud4.png
+	pdflatex Resultados_hw2.tex
+
+Senal.png: Fourier.py
+	python Fourier.py
+
+TransformadasSenales.png:Fourier.py
+	python Fourier.py
+
+EspectrogramaSenales.png:Fourier.py
+	python Fourier.py
